1cdb001c8a327115bbcdf2d084c3208b2e4cebd9
Somil Sharma <somil81@icloud.com>
2023-10-11 23:02:54 +0530

    CORS-links`

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,11 +1,22 @@
-import React from 'react'
-import notes from '../assets/data'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 
 const NotePage = () => {
+
+    let [note, setNote] = useState(null)
+
     const { id } = useParams()
-    const note = notes.find(note => Number(note.id) === Number(id))
+    useEffect(() => {
+        getNote()
+    }, []);
+
+    let getNote = async () => {
+        let response = await fetch(`http://127.0.0.1:8000/api/notes/${id}`)
+        let data = await response.json()
+        setNote(data)
+    }
+
     return (
         <div className="note">
             <div className="note-header">
@@ -15,7 +26,7 @@ const NotePage = () => {
                     </Link>
                 </h3>
             </div>
-            <textarea value={note.body}>
+            <textarea value={note?.body}>
 
             </textarea>
         </div>
--- src/pages/NotesListPage.js
+++ src/pages/NotesListPage.js
@@ -1,9 +1,22 @@
-import notes from '../assets/data'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ListItem from '../components/ListItem'
 
 
 const NotesListPage = () => {
+
+    const [notes, setNotes] = useState([])
+
+    useEffect(() => {
+        getNotes()
+    }, []);
+
+    let getNotes = async () => {
+        let response = await fetch('http://127.0.0.1:8000/api/notes/')
+        let data = await response.json()
+        console.log(data)
+        setNotes(data)
+    }
+
     return (
         <div className='notes'>
             <div className='notes-header'>
